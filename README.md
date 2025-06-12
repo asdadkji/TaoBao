75d9839427ca3bd5778b88f2ad9480ce666bfb37
Li Qi <166194652+asdadkji@users.noreply.github.com>
2025-06-12 22:40:59 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# TaoBao
-这是一个仿淘宝网页版的项目，用于训练前端开发以及后端协调
+# untitled
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ vite.config.js
@@ -0,0 +1,65 @@
+//引入vue
+import vue from '@vitejs/plugin-vue'
+//引入vue-tools
+import VueDevTools from 'vite-plugin-vue-devtools';
+//引入svg
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+//引入mock
+import {viteMockServe} from "vite-plugin-mock";
+//引入格式化检查
+import {defineConfig} from "vite";
+//引入path路径
+import path from 'path';
+//引入全局自动注册组件
+import Components from 'unplugin-vue-components/vite';
+import {AntDesignVueResolver, ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+//引入@符号别名
+import { fileURLToPath, URL } from 'node:url'
+export default defineConfig({
+  plugins: [
+    vue(), VueDevTools(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')], // 指定 SVG 图标文件夹
+      symbolId: 'icon-[dir]-[name]', // 定义 symbolId 格式
+      inject: 'body-last', // DOM 插入位置
+      customDomId: '__svg_icons' // 自定义容器 ID
+    }),
+    viteMockServe({
+      mockPath: 'mock', //指定mock数据存储文件夹
+      enable: process.env.NODE_ENV === 'development', //只在开发环境中自启动
+      logger:true, //是否在控制台显示请求日志
+      watchFiles: true //是否监视mock文件夹的文件更改
+    }),
+    Components({
+      // 配置选项
+      dts: true, // 生成类型声明文件
+      resolvers: [
+        // UI库解析器（如使用Ant Design）
+        AntDesignVueResolver(),
+        ElementPlusResolver({importStyle:'sass'}),//配置ele-plus采用sass
+      ],
+      // 自定义组件目录
+      dirs: ['src/components']
+    }),
+  ],
+  server: {
+    open: true,
+  },
+  resolve:{
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))/*@路径别名*/
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //自动导入定制化样式
+        additionalData:`
+        @use "@/styles/var.scss" as *;
+        `
+      }
+    }
+  }
+})
+
+
